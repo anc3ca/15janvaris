46bb9a9271ff8aec4b61650e0c0e15bd34284575
anc3ca <aa.caune@gmail.com>
2024-01-15 10:29:10 +0200

    aaa

--- 5uzd.py
+++ 5uzd.py
@@ -1,1 +1,3 @@
 # 5.	Izveidot Python programmu, kas ļauj lietotājam ievadīt savu vārdu terminālī. Ievadīto vārdu pēc tam ierakstīt teksta failā (piemēram, "lietotajs.txt"). Programmai jābūt spējīgai apstrādāt kļūdas, piemēram, faila nepieejamību vai rakstīšanas problēmas. Pēc ierakstīšanas izvadīt paziņojumu par veiksmīgu darbību vai kļūdu gadījumā attiecīgu kļūdas ziņojumu. (8 punkti)
+
+input("Ievadi savu vārdu:")
