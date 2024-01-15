350247172b16a0136266191e3d88103e1e10ab78
anc3ca <aa.caune@gmail.com>
2024-01-15 10:09:48 +0200

    1.uzd pabeigts

--- 1uzd.py
+++ 1uzd.py
@@ -2,17 +2,16 @@
 
 
 def read_and_print(file_path):
-    
+
     try:
-        with open(file_path, 'r') as file:
+        with open(file_path, 'r', encoding = 'utf8') as file:
             content = file.read()
-            print("File Content:")
+            print("Datnes saturs:")
             print(content)
     except FileNotFoundError:
-        print(f"Error: File '{file_path}' not found.")
+        print(f"Fails '{file_path}' nav atrasts")
     except Exception as error:
-        print(f"An error occurred: {error}")
+        print(f"Notikusi kļūda: {error}")
 
-# Replace 'your_file.txt' with the actual path of your text file
-file_path = 'your_file.txt'
+file_path = 'fails.txt'
 read_and_print(file_path)
