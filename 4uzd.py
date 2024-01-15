48f88527320f1bffb4c03d3997bba1e8b70a88a7
anc3ca <aa.caune@gmail.com>
2024-01-15 10:27:04 +0200

    4.uzd izpilde

--- 4uzd.py
+++ 4uzd.py
@@ -1,1 +1,26 @@
 # 4.	Izveidot Python programmu, kur  lietotājs ievada par faila nosaukumu un faila formātu (paplašinājumu), un atkarībā no faila paplašinājuma tiek nolasīts faila saturs.  Nolasītā informācija ir jāizdrukā terminālī. Ievērot iespejamās kļūdas! (8 punkti)
+
+
+def read_and_print(file_name, file_format):
+    try:
+        file_path = f"{file_name}.{file_format}"
+
+        with open(file_path, 'r', 'utf-8') as file:
+            content = file.read()
+            print(f"Faila {file_name}.{file_format} saturs:")
+            print(content)
+    except FileNotFoundError:
+        print(f"Error: File '{file_name}.{file_format}' not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+def main():
+
+    file_name = input("Ievadi faila nosaukumu: ")
+    file_format = input("ievadi faila paplašinājumu: ")
+
+
+    read_and_print(file_name, file_format)
+
+if __name__ == "__main__":
+    main()
